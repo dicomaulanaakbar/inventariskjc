a76dedcf20bb142a91d47538af2ba8c6328f5a3b
FadillPro <carleskar276@gmail.com>
2026-04-24 18:39:36 +0700

    ekhem

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // Profile routes (yang sudah ada dari Breeze)
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     
