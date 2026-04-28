1bbd7ae9999774570b635ff9ff33e270d0045209
FadillPro <carleskar276@gmail.com>
2026-04-29 05:48:38 +0700

    total barang

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Barang</div>
-                            {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalBarang }}</div> --}}
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalBarang }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-boxes fa-2x text-gray-300"></i>
